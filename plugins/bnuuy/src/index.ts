2e0fbd7a035005811691de063370c791d7e32336
Ludibrium1 <ludibrium5834@proton.me>
2024-12-25 19:04:07 -0300

    Hopefully fix

--- plugins/bnuuy/src/index.ts
+++ plugins/bnuuy/src/index.ts
@@ -3,9 +3,11 @@ import { before } from "@vendetta/patcher";
 import { Message } from "./def";
 
 export function onLoad() {
+function modify(msg: Message) {
 const messageModule = findByProps("sendMessage", "receiveMessage");
 const uploadModule = findByProps("uploadLocalFiles");
 var newContent = msg.content.replaceAll('([bB])unny', '$1nuuy');
+}
 
 msg.content = newContent;
 

d01ca0be20e7833405007960d518f0ffa281b77c
Ludibrium1 <ludibrium5834@proton.me>
2024-12-25 18:54:15 -0300

    Probably

--- plugins/bnuuy/src/index.ts
+++ plugins/bnuuy/src/index.ts
@@ -2,16 +2,17 @@ import { findByProps } from "@vendetta/metro";
 import { before } from "@vendetta/patcher";
 import { Message } from "./def";
 
+
 const messageModule = findByProps("sendMessage", "receiveMessage");
 const uploadModule = findByProps("uploadLocalFiles");
-
+export function onUnload() {
 var newContent = msg.content.replaceAll('([bB])unny', '$1nuuy');
 
 msg.content = newContent;
 
 const unpatchMessage = before("sendMessage", messageModule, (args) => modify(args[1]));
 const unpatchMessage2 = before("uploadLocalFiles", uploadModule, (args) => modify(args[0].parsedMessage));
-
+}
 export function onUnload() {
     unpatchMessage()
     unpatchMessage2

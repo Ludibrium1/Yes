fb247e09340d49fe9a281f33cebbd92acbcb9339
Ludibrium1 <ludibrium5834@proton.me>
2024-12-25 17:24:15 -0300

    Pray

---
+++ plugins/bnuuy/src/index.ts
@@ -0,0 +1,18 @@
+import { findByProps } from "@vendetta/metro";
+import { before } from "@vendetta/patcher";
+import { Message } from "./def";
+
+const messageModule = findByProps("sendMessage", "receiveMessage");
+const uploadModule = findByProps("uploadLocalFiles");
+
+var newContent = msg.content.replaceAll('([bB])unny', '$1nuuy');
+
+var	msg.content = newContent;
+
+const unpatchMessage = before("sendMessage", messageModule, (args) => modify(args[1]));
+const unpatchMessage2 = before("uploadLocalFiles", uploadModule, (args) => modify(args[0].parsedMessage));
+
+export function onUnload() {
+    unpatchMessage()
+    unpatchMessage2
+}

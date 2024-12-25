49065f4e47c77246c02289b8e3e5a0d45c526367
Ludibrium1 <ludibrium5834@proton.me>
2024-12-25 19:06:44 -0300

    Hopefully fix this time

--- plugins/bnuuy/src/index.ts
+++ plugins/bnuuy/src/index.ts
@@ -2,7 +2,6 @@ import { findByProps } from "@vendetta/metro";
 import { before } from "@vendetta/patcher";
 import { Message } from "./def";
 
-export function onLoad() {
 function modify(msg: Message) {
 const messageModule = findByProps("sendMessage", "receiveMessage");
 const uploadModule = findByProps("uploadLocalFiles");
@@ -13,7 +12,7 @@ msg.content = newContent;
 
 const unpatchMessage = before("sendMessage", messageModule, (args) => modify(args[1]));
 const unpatchMessage2 = before("uploadLocalFiles", uploadModule, (args) => modify(args[0].parsedMessage));
-}
+
 export function onUnload() {
     unpatchMessage()
     unpatchMessage2

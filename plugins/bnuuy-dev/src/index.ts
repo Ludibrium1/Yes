886d9dd9add1fa721819cd44d89d45e76b8ff9a0
Ludibrium1 <ludibrium5834@proton.me>
2024-12-26 11:22:06 -0300

    Revert changes because I have no idea what I'm doing

--- plugins/bnuuy-dev/src/index.ts
+++ plugins/bnuuy-dev/src/index.ts
@@ -4,14 +4,13 @@ import { Message } from "./def";
 
 const messageModule = findByProps("sendMessage", "receiveMessage");
 const uploadModule = findByProps("uploadLocalFiles");
-const regex = "[\* `~_\-\\"'\.,]*";
-const hasBunny = /B${regex}u${regex}n${regex}n${regex}y/i;
+const hasBunny = /B[* `~_\-\\"'\.,]*u[* `~_\-\\"'\.,]*n[* `~_\-\\"'\.,]*n[* `~_\-\\"'\.,]*y/i;
 
 
 function modify(msg: Message) {
     var newContent = msg.content
     if (msg.content.match(hasBunny)) {
-        newContent = msg.content.replaceAll(/(B)(${regex})(u)(${regex})(n)(${regex})(n)(${regex})(y)/gi, '$1$2$5$4$3$6$3$8$9'); }
+        newContent = msg.content.replaceAll(/(B)([* `~_\-\\"'\.,]*)(u)([* `~_\-\\"'\.,]*)(n)([* `~_\-\\"'\.,]*)(n)([* `~_\-\\"'\.,]*)(y)/gi, '$1$2$5$4$3$6$3$8$9'); }
 
 	msg.content = newContent;
 
